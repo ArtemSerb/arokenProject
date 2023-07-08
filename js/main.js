c3109cb0c9a77e2dc9061119d76e42861056a7a6
ArtemSerb <100788768+ArtemSerb@users.noreply.github.com>
2023-07-08 22:15:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function(){
+    var mixer = mixitup('.direction__list');
+    $("#rateYo").rateYo({
+        rating: 4.5
+    });
+    $('#btn').click(function(){
+        $.fancybox.open({
+            src: '#hidden',
+            type: 'inline'
+        });
+    });
+    $('.slider__boxes').slick({
+        arrows: false,
+        slidesToShow: 2,
+        infinite: true,
+        waitForAnimate: false,
+        dots: true,
+        responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+            slidesToShow: 1
+            }
+        }
+        ]
+    })
+    $('.reviews__slider-prev').on('click', function(e) {
+        e.preventDefault()
+        $('.slider__boxes').slick('slickPrev')
+    })
+    $('.reviews__slider-next').on('click', function(e) {
+        e.preventDefault()
+        $('.slider__boxes').slick('slickNext')
+    })
+
+    $('.feedback__link').on('click', function(e) {
+        e.preventDefault()
+        if ($(this).hasClass('feedback__link--active')) {
+        $(this).removeClass('feedback__link--active')
+        $(this).children('.feedback__acc-text').slideUp()
+        } else {
+        $('.feedback__link').removeClass('feedback__link--active')
+        $('.feedback__acc-text').slideUp()
+        $(this).addClass('feedback__link--active')
+        $(this).children('.feedback__acc-text').slideDown()
+        }
+    })
+})
